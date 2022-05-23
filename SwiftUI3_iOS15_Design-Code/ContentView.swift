a91415e4f82b2c0257b5148054dd8a0021a076ae
WENBO HU <darkbrewx@hotmailcom>
2022-05-23 18:50:33 +0800

    Initial Commit

---
+++ SwiftUI3_iOS15_Design-Code/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI3_iOS15_Design-Code
+//
+//  Created by HWB on 2022/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI3_iOS15_Design-Code/SwiftUI3_iOS15_Design_CodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI3_iOS15_Design_CodeApp.swift
+//  SwiftUI3_iOS15_Design-Code
+//
+//  Created by HWB on 2022/5/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI3_iOS15_Design_CodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
